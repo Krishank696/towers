930aa6a3abaec10c24b42248b13e4585e80f5ee0
Krishank696 <73972335+Krishank696@users.noreply.github.com>
2021-03-02 18:46:33 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+  var ground, stand;
+  var b8,b9, b10, b11, b12, b13, b14, b15, b16;
+
+
+
+  ground = new Ground(400, 400);
+  stand = new Ground(200, 200);
+  b8 = new block(330, 235, 30, 40);
+  b9 = new block(360, 235, 30, 40);
+  b10 = new block(390, 235, 30, 40);
+  b11= new block(420, 235, 30, 40);
+  b12= new block(450, 235, 30, 40);
+  b13= new block(360, 195, 30, 40);
+  b14= new block(390, 195, 30, 40);
+  b15 = new block(420, 195, 30, 40);
+  b16= new block(390, 155, 30, 40);
+
+
+  function draw(){
+   background(255);
+   
+
+  }
